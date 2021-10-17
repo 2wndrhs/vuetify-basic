0fd55f68b8c9334dca4298d5c93a00e7342798c0
2wndrhs <2wndrhs@naver.com>
2021-10-17 13:49:49 +0000

    apply scss variables

--- vuetify-admin-template/src/App.vue
+++ vuetify-admin-template/src/App.vue
@@ -68,6 +68,7 @@
 </template>
 
 <script>
+import '@/styles/overrides.sass'
 export default {
   name: 'App',
 
--- vuetify-admin-template/src/plugins/vuetify.js
+++ vuetify-admin-template/src/plugins/vuetify.js
@@ -1,7 +1,17 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
-
 Vue.use(Vuetify);
 
+const theme = {
+    primary: '#42b883',
+    secondary: '#35495e'
+}
+
 export default new Vuetify({
+    theme: {
+        themes: {
+            light: theme,
+            dark: theme
+        }
+    }
 });
