6bf7b85b87bfd28f069fadca700e2f4e3f2486c2
2wndrhs <2wndrhs@naver.com>
2021-11-02 14:56:41 +0000

    nav depth

--- vuetify-admin-template/src/layouts/default/Drawer.vue
+++ vuetify-admin-template/src/layouts/default/Drawer.vue
@@ -15,50 +15,34 @@
         >
         </v-img>
       </template>
-        
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title class="text-h6">
-            Application
-          </v-list-item-title>
-          <v-list-item-subtitle>
-            subtext
-          </v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
 
+      <default-drawer-header></default-drawer-header>
+        
       <v-divider></v-divider>
 
-      <v-list
-        dense
-        nav
-      >
-        <v-list-item
-          v-for="item in items"
-          :key="item.title"
-          link
-          :to="item.to"
-          active-class="primary"
-          class="py-1"
-        >
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
+      <default-list :items="items"></default-list>
+      
     </v-navigation-drawer>
 </template>
 
 <script>
+import DefaultDrawerHeader from './DrawerHeader.vue';
+import DefaultList from './List.vue';
 export default {
     name: "DefaultDrawer",
+    components: {
+      DefaultDrawerHeader,
+      DefaultList
+    },
     data(){
         return {
             items: [
             { title: 'Dashboard', icon: 'mdi-view-dashboard', to: '/' },
+            { title: 'Pages', icon: 'mdi-menu', items: [
+              { title: 'Signin', icon: 'mdi-login', to: '/authentication/sign-in'},
+              { title: 'Signup', icon: 'mdi-logout', to: '/authentication/sign-up'},
+              { title: 'ProductList', icon: 'mdi-reproduction', to: '/page/product-list'},
+            ]},
             { title: 'Grid System', icon: 'mdi-view-dashboard', to: '/grid-system'},
             { title: 'Grid List Page', icon: 'mdi-view-dashboard', to: '/grid-list-page'},
             { title: 'Breakpoints', icon: 'mdi-view-dashboard', to: '/breakpoints'},
@@ -67,9 +51,6 @@ export default {
             { title: 'Forms', icon: 'mdi-view-dashboard', to: '/forms'},
             { title: 'Buttons', icon: 'mdi-view-dashboard', to: '/buttons'},
             { title: 'Icons', icon: 'mdi-view-dashboard', to: '/icons'},
-            { title: 'Signin', icon: 'mdi-view-dashboard', to: '/authentication/sign-in'},
-            { title: 'Signup', icon: 'mdi-view-dashboard', to: '/authentication/sign-up'},
-            { title: 'ProductList', icon: 'mdi-view-dashboard', to: '/page/product-list'},
             ],
             gradient: 'rgba(0, 0, 0, .7), rgba(0, 0, 0, .7)',
         }
---
+++ vuetify-admin-template/src/layouts/default/List.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-list
+    dense
+    nav
+    >
+        <template
+            v-for="(item, index) in items"
+        >
+            <default-list-item
+                :key="index"
+                :item="item"
+            >
+            </default-list-item>
+        </template>
+    </v-list>
+</template>
+
+<script>
+import DefaultListItem from './ListItem.vue'
+export default {
+name: 'DefaultList',
+components: {
+    DefaultListItem
+},
+props: {
+    items: {
+        type: Array,
+        default: () => ([]),
+    }
+}
+}
+</script>
+
+<style>
+
+</style>
